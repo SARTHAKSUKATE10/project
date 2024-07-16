f5c637fc6d478ba086936900012897be9a9e1be3
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2024-07-16 21:18:27 +0530

    create, read and delete feature added

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:3000/testapp1")
+mongoose.connect("mongodb://127.0.0.1:27017/testapp1")
 
 const userSchema = mongoose.Schema({
     name: String,
